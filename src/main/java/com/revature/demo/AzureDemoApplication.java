dcd07a3a890698cf91ef338eba826d45ff4ad916
Jack <iatenine@gmail.com>
2022-03-14 13:34:57 -0600

    add @ComponentScan

--- src/main/java/com/revature/demo/AzureDemoApplication.java
+++ src/main/java/com/revature/demo/AzureDemoApplication.java
@@ -2,8 +2,10 @@ package com.revature.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.revature.demo")
 public class AzureDemoApplication {
 
 	public static void main(String[] args) {
