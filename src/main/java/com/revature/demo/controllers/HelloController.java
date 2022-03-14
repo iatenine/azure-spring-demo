01253dd8f9e17d72dbfa5f3b2b86964ac169b406
RoryEiffe <roryeiffe8@gmail.com>
2022-03-14 12:13:21 -0400

    Create empty spring boot project.

---
+++ src/main/java/com/revature/demo/controllers/HelloController.java
@@ -0,0 +1,18 @@
+package com.revature.demo.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello from Spring Boot App!";
+    }
+
+    @GetMapping("/fruit")
+    public String fruit() {
+        return "apple banana kiwi orange";
+    }
+}
